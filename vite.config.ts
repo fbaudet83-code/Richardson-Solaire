f98be672cd972268712ed022e4fd07240a1d3efb
fbaudet83-code <fbaudet83@gmail.com>
2025-12-07 15:14:47 +0100

    Update Vite config to set root and emptyOutDir

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // La racine est le dossier courant
+  root: '.',
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
   }
 })
-
